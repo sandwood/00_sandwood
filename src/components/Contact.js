41ca43fb113e195dc1bfa4067aad9943cb90fec3
DESKTOP-SD5KS9S\SANDWOOD <sandwood09@gmail.com>
2017-10-24 11:50:53 +0900

    Initial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import ContactInfo from './ContactInfo';
+import ContactDetails from './ContactDetails';
+import update from 'react-addons-update';
+
+class Contact extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedKey: -1,
+            keyword: '',
+            contactData: [{
+                name: 'Abet',
+                phone: '010-0000-0001'
+            }, {
+                name: 'Betty',
+                phone: '010-0000-0002'
+            }, {
+                name: 'Charlie',
+                phone: '010-0000-0003'
+            }, {
+                name: 'David',
+                phone: '010-0000-0004'
+            }]
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(key){
+        this.setState({
+            selectedKey: key
+        });
+        console.log(key, 'is selected');
+    }
+    handleChange(e){
+        this.setState({
+            keyword: e.target.value
+        })
+    }
+    render() {
+        const mapToComponents = (data) => {
+            data.sort();
+            data = data.filter(
+                (contact) => {
+                    return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;
+                }
+            );
+            return data.map((contact, i) => {
+                return (
+                    <ContactInfo
+                        contact={contact} 
+                        key={i}
+                        onClick={() => this.handleClick(i)}
+                    />
+                );
+            });
+        };
+
+        return (
+            <div>
+                <h1>Contacts</h1>
+                <input 
+                    name="keyword"
+                    placeholder="Search"
+                    value={this.state.keyword}
+                    onChange={this.handleChange}
+                />
+                <div>{mapToComponents(this.state.contactData)}</div>
+                <ContactDetails 
+                    isSelected={this.state.selectedKey != -1}
+                    contact={this.state.contactData[this.state.selectedKey]}
+                />
+            </div>
+        );
+    }
+}
+
+export default Contact;
